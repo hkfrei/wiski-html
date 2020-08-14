3b083bfa01d994dfca473fcfb32219b9bb0407ce
Hanskaspar Frei <hkfrei@karten-werk.ch>
2020-08-14 13:34:36 +0200

    feat:basic project structure setup

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // load the things we need
 var express = require("express");
 var app = express();
-var path = require("path");
 // set the view engine to ejs
 app.set("view engine", "ejs");
 
@@ -12,25 +11,28 @@ app.get("/", function (req, res) {
   // public:      allow caching on a server.
   // max-age:     how long can content be stored in users browsers.
   // s-maxage:    how long can content be stored on a cdn.
-  //res.set("Cache-Control", "public, max-age=300, s-maxage=600s");
-  var drinks = [
-    { name: "Bloody Mary", drunkness: 3 },
-    { name: "Martini", drunkness: 5 },
-    { name: "Scotch", drunkness: 10 },
-  ];
-  var tagline =
-    "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
-
+  res.set("Cache-Control", "public, max-age=300, s-maxage=600s");
+  var endpoints = ["/wasser?id=123", "/boden?id=123"];
   res.render("pages/index", {
-    drinks: drinks,
-    tagline: tagline,
+    endpoints: endpoints,
   });
 });
 
-// about page
-app.get("/about", function (req, res) {
-  res.render("pages/about");
+// wasser page
+app.get("/wasser", function (req, res) {
+  const { stationid } = req.query;
+  res.render("pages/wasser", { station: stationid });
 });
+// boden page
+app.get("/boden", function (req, res) {
+  const { stationid } = req.query;
+  res.render("pages/boden", { station: stationid });
+});
+// not found
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry this route does not exist!");
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port);
 console.log(`${port} is the magic port`);

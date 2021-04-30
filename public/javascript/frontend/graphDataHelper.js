7651691e0b5696efa52c6114b8beed3e751399d9
Hanskaspar Frei <hkfrei@karten-werk.ch>
2021-04-30 10:22:55 +0200

    feat:implement a downsampling method in order we don't have to use the plugin which is deprecated.

--- public/javascript/frontend/graphDataHelper.js
+++ public/javascript/frontend/graphDataHelper.js
@@ -38,7 +38,7 @@ const graphDataHelper = {
    * @param {array} params.data - time-series data from kiwis.
    * @returns {object} result - { labels:['the labels'], data:['chart.js optimized data'] }.
    */
-  prepStationData: function ({ data } = {}) {
+  prepStationData: function ({ data, threshold = 50 } = {}) {
     const result = { labels: [], data: [] };
     for (const element of data) {
       // don't add falsy values except 0
@@ -48,9 +48,11 @@ const graphDataHelper = {
       const date = new Date(element[0]);
       // use absolute values where available
       const value = element[2] ? element[2] : element[1];
-      result.labels.push(date);
       result.data.push({ x: date, y: value });
     }
+    const downsampled = this.downsample(result.data, threshold);
+    result.labels = downsampled.map((element) => element.x);
+    result.data = downsampled;
     return result;
   },
 
@@ -85,6 +87,78 @@ const graphDataHelper = {
       }
     }
     return result;
+  },
+  downsample: function (data, threshold) {
+    // this function is from flot-downsample (MIT), with modifications
+
+    var dataLength = data.length;
+    if (threshold >= dataLength || threshold <= 0) {
+      return data; // nothing to do
+    }
+
+    var sampled = [],
+      sampledIndex = 0;
+
+    // bucket size, leave room for start and end data points
+    var every = (dataLength - 2) / (threshold - 2);
+
+    var a = 0, // initially a is the first point in the triangle
+      maxAreaPoint,
+      maxArea,
+      area,
+      nextA;
+
+    // always add the first point
+    sampled[sampledIndex++] = data[a];
+
+    for (var i = 0; i < threshold - 2; i++) {
+      // Calculate point average for next bucket (containing c)
+      var avgX = 0,
+        avgY = 0,
+        avgRangeStart = Math.floor((i + 1) * every) + 1,
+        avgRangeEnd = Math.floor((i + 2) * every) + 1;
+      avgRangeEnd = avgRangeEnd < dataLength ? avgRangeEnd : dataLength;
+
+      var avgRangeLength = avgRangeEnd - avgRangeStart;
+
+      for (; avgRangeStart < avgRangeEnd; avgRangeStart++) {
+        avgX += data[avgRangeStart].x * 1; // * 1 enforces Number (value may be Date)
+        avgY += data[avgRangeStart].y * 1;
+      }
+      avgX /= avgRangeLength;
+      avgY /= avgRangeLength;
+
+      // Get the range for this bucket
+      var rangeOffs = Math.floor((i + 0) * every) + 1,
+        rangeTo = Math.floor((i + 1) * every) + 1;
+
+      // Point a
+      var pointAX = data[a].x * 1, // enforce Number (value may be Date)
+        pointAY = data[a].y * 1;
+
+      maxArea = area = -1;
+
+      for (; rangeOffs < rangeTo; rangeOffs++) {
+        // Calculate triangle area over three buckets
+        area =
+          Math.abs(
+            (pointAX - avgX) * (data[rangeOffs].y - pointAY) -
+              (pointAX - data[rangeOffs].x) * (avgY - pointAY)
+          ) * 0.5;
+        if (area > maxArea) {
+          maxArea = area;
+          maxAreaPoint = data[rangeOffs];
+          nextA = rangeOffs; // Next a is this b
+        }
+      }
+
+      sampled[sampledIndex++] = maxAreaPoint; // Pick this point from the bucket
+      a = nextA; // This a is the next a (chosen b)
+    }
+
+    sampled[sampledIndex] = data[dataLength - 1]; // Always add last
+
+    return sampled;
   },
 };
 
